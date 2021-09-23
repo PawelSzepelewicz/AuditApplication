65fef00f07c069feeb842e235e85db2bca69f51e
Pawel Szepelewicz <pashashepel1997@outlook.com>
2021-09-23 17:32:20 +0300

    Added testing of controllers, services, repositories, kafka

--- src/main/java/com/example/audit/service/impl/KafkaConsumerServiceImpl.java
+++ src/main/java/com/example/audit/service/impl/KafkaConsumerServiceImpl.java
@@ -5,14 +5,12 @@ import com.example.audit.service.KafkaConsumerService;
 import com.example.audit.service.LogsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@Component
 public class KafkaConsumerServiceImpl implements KafkaConsumerService {
-   private final LogsService logsService;
+    private final LogsService logsService;
 
     @Override
     @KafkaListener(topics = "spring-audit", groupId = "probation-audit", containerFactory = "kafkaListenerContainerFactory")
